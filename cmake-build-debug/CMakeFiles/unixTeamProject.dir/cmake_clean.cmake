c55b4ffdad5d34637c690fb7e534e6e51c2c7b1e
guswn <guswns3371@gmail.com>
2019-11-27 19:59:11 +0900

    unixTeamProject

---
+++ cmake-build-debug/CMakeFiles/unixTeamProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unixTeamProject.dir/main.c.obj"
+  "libunixTeamProject.dll.a"
+  "unixTeamProject.exe"
+  "unixTeamProject.exe.manifest"
+  "unixTeamProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/unixTeamProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
